906e9511637e7689ede3845bdb7ba9add6487f7f
woow_wu7 <woow.wu7@gmail.com>
2021-12-31 17:36:58 +0800

    feat: review promsie2.

--- JS/3-promise/index.js
+++ JS/3-promise/index.js
@@ -48,13 +48,13 @@ class _Promise {
         // setTimeout(() => { // 这里不用setTimeout包装，是因为在Promise的constructor中本来就存在异步，resolve的执行可定在同步任务之后，而then回调是在resolve中执行的
         //   onFulfilled(this.value);
         // })
-        (value) => onFulfilled(value)
+        (value) => setTimeout(() => onFulfilled(value))
       );
       this.onRejectedCallbacks.push(
         // setTimeout(() => {
         //   onRejected(this.reason);
         // })
-        (reason) => onRejected(reason)
+        (reason) => setTimeout(() => onRejected(reason))
       );
     }
   };
@@ -65,16 +65,30 @@ const promise2 = new _Promise((resolve, reject) => {
   console.log(2);
   setTimeout(() => {
     resolve("Promise");
+    console.log(4);
   });
 }).then((value) => {
-  console.log(4);
+  console.log(5);
 });
 console.log(3);
 
-// const promise1 = new Promise((resolve, reject) => {
-//   console.log("Promise");
-//   resolve("Promise");
-// }).then(
-//   (value) => console.log("终值", value),
-//   (reason) => console.log("拒因", reason)
-// );
+// ------------- 分割线 -------------
+// ------------- 分割线 -------------
+// ------------- 分割线 -------------
+// 原生 promise 的执行顺序
+console.log("aaa");
+const promise1 = new Promise((resolve, reject) => {
+  console.log("bbb");
+  setTimeout(() => {
+    resolve("Promise");
+    console.log("ddd"); // promise中异步，同时异步中有同步时，也要保证then的执行在异步任务中的同步任务执行完成后，再执行
+  });
+}).then(
+  (value) => {
+    console.log("eee");
+  },
+  (reason) => console.log("拒因", reason)
+);
+console.log("ccc");
+// 执行顺序 a b c d e
+// 注意：ddd 和 eee 的执行顺序，要保证 ddd先执行
