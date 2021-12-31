f760c2a9d5e3d9cfcd0c879cf8eaa30ed0b2ae5a
woow_wu7 <woow.wu7@gmail.com>
2021-12-31 21:59:13 +0800

    feat: review manual promsie.all.

--- JS/3-promise/index.js
+++ JS/3-promise/index.js
@@ -15,7 +15,12 @@ class _Promise {
         `Promise resolver ${executor} is not a function` // 因为不是函数时，是可以直接获取的
       );
 
-    executor(this.resolve, this.reject);
+    // try...catch 是为了保证在promise内部发生错误时，能在内部被捕获
+    try {
+      executor(this.resolve, this.reject);
+    } catch (err) {
+      this.reject(err);
+    }
   }
 
   resolve = (value) => {
@@ -28,13 +33,22 @@ class _Promise {
   reject = (reason) => {
     if (this.status === this.PENDING) {
       this.status = this.REJECTED;
-      this.value = reason;
+      this.reason = reason;
     }
   };
 
   then = (onFulfilled, onRejected) => {
+    // 穿透效果
+    if (typeof onFulfilled !== "function") {
+      onFulfilled = (value) => value;
+    }
+    if (typeof onRejected !== "function") {
+      onRejected = (reason) => reason;
+    }
+
     if (this.status === this.FULFILLED) {
       setTimeout(() => {
+        // 保证了then的onFulfilled参数函数在同步任务执行完后，再执行
         onFulfilled(this.value);
       });
     }
@@ -45,33 +59,72 @@ class _Promise {
     }
     if (this.status === this.PENDING) {
       this.onFulfilledCallbacks.push(
-        // setTimeout(() => { // 这里不用setTimeout包装，是因为在Promise的constructor中本来就存在异步，resolve的执行可定在同步任务之后，而then回调是在resolve中执行的
-        //   onFulfilled(this.value);
-        // })
         (value) => setTimeout(() => onFulfilled(value))
         // 这里把onFulfilled再包装一层的原因是：可以传入参数，而参数正是终值
       );
-      this.onRejectedCallbacks.push(
-        // setTimeout(() => {
-        //   onRejected(this.reason);
-        // })
-        (reason) => setTimeout(() => onRejected(reason))
+      this.onRejectedCallbacks.push((reason) =>
+        setTimeout(() => onRejected(reason))
       );
     }
   };
 }
 
+_Promise.all = (promiseList) => {
+  // Promise.all
+  // 1. 是静态方法
+  // 2. 返回一个新的 promise
+  return new Promise((resolve, reject) => {
+    const promiseArr = [...promiseList];
+    const len = promiseList.length;
+    const promiseValueResult = []; // 用来存储参数数组成员转成promise对象后的，fulfilled状态的所有终值
+    promiseArr.forEach((promise) => {
+      Promise.resolve(promise).then((value) => {
+        promiseValueResult.push(value);
+        if (promiseValueResult.length === len) {
+          return resolve(promiseValueResult); // aa. 所有都是fulfilled状态，就resolve整个终值数组
+        }
+      }, reject); // --------------------------- bb. 如果一个promise状态变成rejected，整个就rejected
+    });
+  });
+};
+
+// ------------- 分割线 -------------
+// ------------- 分割线 -------------
+// ------------- 分割线 -------------
+// promise的执行顺序
 console.log(1);
 const promise2 = new _Promise((resolve, reject) => {
   console.log(2);
   setTimeout(() => {
-    resolve("Promise");
     console.log(4);
+    resolve("Promise");
+    console.log(5);
+    // 1. 当 resolve 函数后面还有代码时，还是会往下执行，除非有 return resolve("Promise")
+    // 2. 并且还要保证 resolve 后面的同步任务在then的回调函数执行前执行
   });
-}).then((value) => {
-  console.log(5);
-});
+}).then(
+  (value) => {
+    console.log(6);
+  },
+  (reason) => {
+    console.log(`reason`, reason);
+  }
+);
 console.log(3);
+
+// ------------- 分割线 -------------
+// ------------- 分割线 -------------
+// ------------- 分割线 -------------
+// test Promise.all()
+const promiseX = new _Promise((resolve) => resolve("x"));
+const promiseY = new _Promise((resolve, rejected) => {
+  resolve("y");
+  // rejected(1);
+});
+const all = _Promise.all([promiseX, promiseY]).then(
+  (values) => console.log(`values`, values),
+  (reason) => console.log(`reason`, reason)
+);
 
 // ------------- 分割线 -------------
 // ------------- 分割线 -------------
@@ -81,12 +134,13 @@ console.log("aaa");
 const promise1 = new Promise((resolve, reject) => {
   console.log("bbb");
   setTimeout(() => {
+    console.log("ddd");
     resolve("Promise");
-    console.log("ddd"); // promise中异步，同时异步中有同步时，也要保证then的执行在异步任务中的同步任务执行完成后，再执行
+    console.log("eee"); // promise中异步，同时异步中有同步时，也要保证then的执行在异步任务中的同步任务执行完成后，再执行
   });
 }).then(
   (value) => {
-    console.log("eee");
+    console.log("fff");
   },
   (reason) => console.log("拒因", reason)
 );
